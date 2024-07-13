4b07db41509b651d6790d70315b90ede798c4085
“Noah_Meakins” <unknownerror66@outlook.com>
2024-07-13 00:42:16 -0600

    modified save path due to project root resolution

--- code/load_data.py
+++ code/load_data.py
@@ -3,19 +3,19 @@ import pandas as pd
 import sqlite3
 
 # loading bom_movie_gross from source file
-def load_bom_movie_gross(filepath='zippedData/bom.movie_gross.csv.gz'):
+def load_bom_movie_gross(filepath='../zippedData/bom.movie_gross.csv.gz'):
     return pd.read_csv(filepath, compression='gzip')
 
 #loading tmdb_movies from source file
-def load_tmdb_movies(filepath="zippedData/tmdb.movies.csv.gz"):
+def load_tmdb_movies(filepath='../zippedData/tmdb.movies.csv.gz'):
     return pd.read_csv(filepath, compression='gzip')
 
 # loading tn_movie_budgets from source file
-def load_tn_movie_budgets(filepath='zippedData/tn.movie_budgets.csv.gz'):
+def load_tn_movie_budgets(filepath='../zippedData/tn.movie_budgets.csv.gz'):
     return pd.read_csv(filepath, compression='gzip')
 
 # loading the im.db database from source file
-def load_sqlite_db(filepath='zippedData/im.db'):
+def load_sqlite_db(filepath='../zippedData/im.db'):
     # establishing a connection to the SQLite database
     conn = sqlite3.connect(filepath)
     # reading movie_basics table into a DataFrame
